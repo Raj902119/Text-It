7e37fdd765a83aa2865dbb6278f337df619bfb6e
Rajwardhan Patil <rajpatil2k@gmail.com>
2024-04-16 14:20:50 +0530

    upgraded config

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,6 @@
 const corsOptions = {
     origin: [
-        "http://localhost:5173",
-        "http://localhost:4173",
+        "https://text-it-frontend.vercel.app",
         process.env.CLIENT_URL,
     ],
     credentials: true,

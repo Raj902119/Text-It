c19929a49896b9d930a8b98057bff84f358cc90a
Rajwardhan Patil <rajpatil2k@gmail.com>
2024-04-16 20:57:37 +0530

    hope final

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     'Access-Control-Allow-Origin': process.env.CLIENT_URL,
-    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Credentials': false,
     'Access-Control-Allow-Methods': ["GET","POST","PUT","DELETE"],
 };
 
